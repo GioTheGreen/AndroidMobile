27999032328e083bbcad63697a168f7da81aa66c
GioTheGreen <B1050622@live.tees.ac.uk>
2023-11-27 00:46:50 +0000

    setful
    up to 6:21

--- first/app/src/main/java/uk/ac/uk/tees/mgd/B1050622/sakithslookatandraid/GameView.java
+++ first/app/src/main/java/uk/ac/uk/tees/mgd/B1050622/sakithslookatandraid/GameView.java
@@ -14,82 +14,44 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GameView extends SurfaceView implements Runnable {
-
-    //private Thread gameThread;
-    private SurfaceHolder surfaceHolder;
-    private volatile boolean playing;
-    private Canvas canvas;
-    private Bitmap bitmap;
-    private boolean isMoving;
-    //private float velocity = 250; // 250 px/s
-    //private float xPos = 10, yPos = 10;
-    //private int frameW = 115, frameH = 137;
-    //private int frameCount = 8;
-    //private int currentFrame = 0;
-    private long fps;
-    private long timeThisFrame;
-    private long lastFrameChangeTime = 0;
-    //private int frameLengthInMS = 100;
-    //private Rect frameToDraw = new Rect(0,0,frameW,frameH);
-    //private RectF whereToDraw = new RectF(xPos, yPos, xPos + frameW, frameH);
+    private boolean playing = true;
 
     public GameView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
-        surfaceHolder = getHolder();
-        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.run);
-        //bitmap = Bitmap.createScaledBitmap(bitmap, frameW * frameCount, frameH, false);
+
     }
 
+    private class Anim extends Thread{
+        int counter = 0;
+
+        @Override
+        public void run() {
+            long LastFrameTime=0;
+            long Delay =100;
+            int spriteIDs[]={
+                    //R.drawable.demoP
+            };
+        }
+    }
 
     @Override
     public void run() {
-        //while (playing)
-        //{
-        draw();
-        //}
     }
 
-    private void update() {
+    private void GameUpdate() {
 
     }
 
 
-
-    public void draw() {
-        if (surfaceHolder.getSurface().isValid())
-        {
-            canvas = surfaceHolder.lockCanvas();
-            canvas.drawColor(Color.WHITE);
-
-            surfaceHolder.unlockCanvasAndPost(canvas);
-        }
-    }
-
-    //public void pause()
-    //{
-       // playing = false;
-        //try {
-            //gameThread.join();
-        //} catch (InterruptedException e) {
-            //Log.e("GameView", "Interrupted");
-        //}
-    //}
-
-    //public void resume()
-    //{
-       // playing = true;
-        //gameThread = new Thread(this);
-        //gameThread.start();
-    //}
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction() & MotionEvent.ACTION_MASK) {
-            case MotionEvent.ACTION_DOWN :
-                isMoving = !isMoving;
-                Log.d("GameLayout", "" + isMoving);
-                break;
-        }
+        //switch (event.getAction() & MotionEvent.ACTION_MASK) {
+        //    case MotionEvent.ACTION_DOWN :
+        //        isMoving = !isMoving;
+        //        Log.d("GameLayout", "" + isMoving);
+        //        break;
+        //}
         return true;
     }
 }
